16e54fb5a45fa94406928f187421fc8d75d9f178
CC-3636 <gameshunn@gmail.com>
2019-02-15 10:38:02 +0100

    Fixed saving of FloodMode information

--- src/main/java/io/github/randalf/project/commands/ArenaSetFloodModeEntityAmountCommand.java
+++ src/main/java/io/github/randalf/project/commands/ArenaSetFloodModeEntityAmountCommand.java
@@ -1,5 +1,6 @@
 package io.github.randalf.project.commands;
 
+import io.github.randalf.project.arenaparts.Arena;
 import io.github.randalf.project.arenaparts.spawner.FloodMode;
 import io.github.randalf.project.manager.ArenaManager;
 import org.spongepowered.api.command.CommandException;
@@ -21,6 +22,8 @@ public class ArenaSetFloodModeEntityAmountCommand implements CommandExecutor {
         String arenaName = args.<String>getOne("arenaName").get();
         String entityAmount = args.<String>getOne("entityAmount").get();
         ((FloodMode) ArenaManager.getInstance().getArena(arenaName).getMode()).setEntityAmount(Integer.parseInt(entityAmount));
+        Arena arena = ArenaManager.getInstance().getArena(arenaName);
+        ArenaManager.getInstance().saveArena(arenaName, arena);
         return CommandResult.success();
     }
 }
--- src/main/java/io/github/randalf/project/commands/ArenaSetFloodModeEntityTypeCommand.java
+++ src/main/java/io/github/randalf/project/commands/ArenaSetFloodModeEntityTypeCommand.java
@@ -1,5 +1,6 @@
 package io.github.randalf.project.commands;
 
+import io.github.randalf.project.arenaparts.Arena;
 import io.github.randalf.project.arenaparts.spawner.FloodMode;
 import io.github.randalf.project.manager.ArenaManager;
 import org.spongepowered.api.command.CommandException;
@@ -21,6 +22,8 @@ public class ArenaSetFloodModeEntityTypeCommand implements CommandExecutor {
         String arenaName = args.<String>getOne("arenaName").get();
         String entityType = args.<String>getOne("entityType").get();
         ((FloodMode)ArenaManager.getInstance().getArena(arenaName).getMode()).setEntityType(entityType);
+        Arena arena = ArenaManager.getInstance().getArena(arenaName);
+        ArenaManager.getInstance().saveArena(arenaName, arena);
         return CommandResult.success();
     }
 }
