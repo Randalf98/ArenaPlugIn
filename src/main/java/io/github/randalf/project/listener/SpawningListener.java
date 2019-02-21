ac502d5a301b3117b7577a774c8cbcff909a663d
Randalf98 <gameshunn+github@gmail.com>
2019-02-21 13:25:13 +0100

    Recognition of further killing methods

--- src/main/java/io/github/randalf/project/listener/SpawningListener.java
+++ src/main/java/io/github/randalf/project/listener/SpawningListener.java
@@ -4,8 +4,11 @@ import com.flowpowered.math.vector.Vector3i;
 import io.github.randalf.project.arenaparts.Area;
 import io.github.randalf.project.arenaparts.spawner.ArenaSpawner;
 import org.spongepowered.api.entity.Entity;
+import org.spongepowered.api.entity.EntityTypes;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.cause.entity.damage.source.EntityDamageSource;
+import org.spongepowered.api.event.cause.entity.damage.source.IndirectEntityDamageSource;
 import org.spongepowered.api.event.entity.DestructEntityEvent;
 
 public class SpawningListener extends ArenaListener {
@@ -22,13 +25,20 @@ public class SpawningListener extends ArenaListener {
     @Listener
     public void onDeadEntity(DestructEntityEvent event) {
         Entity  e = event.getTargetEntity();
-        if(spawner.getEntitiesList().contains(e) && event.getCause().first(Player.class).isPresent()){
-            Player player = event.getCause().first(Player.class).get();
-            Vector3i playerPosition = player.getLocation().getChunkPosition();
-            if (area.contains(playerPosition)){
-                spawner.spawnEnemys();
+        if(spawner.getEntitiesList().contains(e) && (event.getCause().first(EntityDamageSource.class).isPresent() || event.getCause().first(IndirectEntityDamageSource.class).isPresent())){
+            Player player = null;
+            if(event.getCause().first(IndirectEntityDamageSource.class).isPresent()){
+                player = (Player)event.getCause().first(IndirectEntityDamageSource.class).get().getIndirectSource();
+            } else if (event.getCause().first(EntityDamageSource.class).get().getSource().getType().equals(EntityTypes.PLAYER)){
+                player = (Player)event.getCause().first(EntityDamageSource.class).get().getSource();
             }
-            spawner.setLastDiedEntity(e);
+            if(player != null){
+                Vector3i playerPosition = player.getLocation().getChunkPosition();
+                if (area.contains(playerPosition)){
+                    spawner.spawnEnemys();
+                }
+                spawner.setLastDiedEntity(e);
+            }
         }
         try{
             spawner.getEntitiesList().remove(e);
